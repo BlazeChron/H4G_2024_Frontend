31578c26fab59dbbc5eb3e345546577f8d261405
BlazeChron <jasonjhl20@gmail.com>
2024-02-06 11:19:27 +0800

    routes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <template>
-  <div>
-    <SignUp />
-    <SignIn />
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> | 
+    <router-link to="/signup">Sign Up</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import SignIn from './components/SignIn.vue';
-import SignUp from './components/SignUp.vue';
-
-export default {
-  name: 'App',
-  components: {
-    SignIn,
-    SignUp,
-  }
-}
-</script>
 
 <style>
 #app {
@@ -25,6 +14,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
