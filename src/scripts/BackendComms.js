5fd0199c19af83ea24de6ed9d87221d1e602e013
BlazeChron <jasonjhl20@gmail.com>
2024-02-08 16:08:08 +0800

    Dashboard.vue

--- src/scripts/BackendComms.js
+++ src/scripts/BackendComms.js
@@ -2,75 +2,66 @@
 import Cookies from 'js-cookie';
 const baseURL = "http://localhost:3000";
 
+const baseRequest = (url, method, body, includeJWT) => {
+  
+  const storedToken = includeJWT ? Cookies.get('jwtToken') : ""; 
+
+  const request = new Request(baseURL + url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+      'Authorization': `Bearer ${storedToken}`
+    },
+    body: JSON.stringify(body),
+  }
+  );
+
+  return request;
+}
+
 const userSignUp = (username, email, password) => {
-  fetch(baseURL + "/user/signup",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-          username: username,
-          email: email,
-          password: password, 
-      }),
-    }
-  ).catch(error => {
-                 console.error(error);
-                   });
+  const body = {
+    username: username,
+    email: email,
+    password: password, 
+  }
+
+  fetch(baseRequest("/user/signup", "POST", body, false))
+        .catch(error => {console.error(error);});
 }
 
 const userSignIn = (username, email, password) => {
-  console.log("signin");
-  fetch(baseURL + "/user/signin",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-          username: username,
-          email: email,
-          password: password, 
-      }),
-    }
-  )
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-  })
-  .then((response) => {
-    console.log(response.token);
-    Cookies.set('jwtToken', response.token, { expires: 1 }); // Set the cookie to expire in 1 day 
-  })
-  .catch(error => {
-                 console.error(error);
-                   });
+
+  const body = {
+    username: username,
+    email: email,
+    password: password, 
+  }
+
+  fetch(baseRequest("/user/signin", "POST", body, false))
+        .then((response) => {
+              if (response.ok) {
+                return response.json();
+              }
+        })
+        .then((response) => {
+              console.log(response.token);
+              Cookies.set('jwtToken', response.token, { expires: 1 }); // Set the cookie to expire in 1 day 
+        })
+        .catch(error => {console.error(error);});
 }
 
 const verifyJWT = () => {
-  console.log("signincheck");
-  const storedToken = Cookies.get('jwtToken');
-  fetch(baseURL + "/user/signincheck",
-    {
-      method: "POST",
-      headers: {
-        'Authorization': `Bearer ${storedToken}`
-      },
-    }
-  )
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-  })
-  .then((response) => {
-    console.log(response.token);
-  })
-  .catch(error => {
-                 console.error(error);
-                   });
+  fetch(baseRequest("/user/signincheck", "POST", {}, true))
+        .then((response) => {
+              if (response.ok) {
+                return response.json();
+              }
+        })
+        .then((response) => {
+              console.log(response.token);
+        })
+        .catch(error => {console.error(error);});
 
 return;
 }
